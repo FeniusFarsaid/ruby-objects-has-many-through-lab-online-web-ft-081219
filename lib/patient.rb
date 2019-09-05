6217e9d349564ee84004ce4cdd88791ff3f664b6
David Ingram <david@ingramaviation.com>
2019-09-05 15:08:47 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,12 +2,15 @@ class Patient
   
   attr_accessor :name 
   
+  @@all = []
+  
   def initialize(name)
     @name = name
+    @@all << self 
   end 
   
   def self.all 
-    
+    @@all
   end 
   
   def new_appointment(date, doctor)
